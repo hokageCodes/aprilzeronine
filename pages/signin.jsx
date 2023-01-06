26fc10d578b591f88cd06b225d05ab8e1c1d7876
hokageCodes <hashsenju94@gmail.com>
2023-01-06 18:51:39 +0100

    fridayyyyyy

--- pages/signin.jsx
+++ pages/signin.jsx
@@ -1,7 +1,10 @@
 import styles from '../styles/signin.module.scss';
 import Footer from '../components/footer'
 import Header from '../components/header'
-import BiLeftArrowAlt from 'react-icons/bi'
+import {BiLeftArrowAlt} from 'react-icons/bi'
+import Link from 'next/link'
+import { Form, Formik } from 'formik';
+import LoginInput from '../components/inputs/loinInput/loginInput';
 
 export default function signin() {
   return (
@@ -13,6 +16,20 @@ export default function signin() {
             <div className={styles.back__svg}>
               <BiLeftArrowAlt />
             </div>
+            <span>Please Join Us !</span> <Link href="/">Go to Sore</Link>
+          </div>
+          <div className={styles.login__form}>
+            <h1>Sign In</h1>
+            <p>Get access to the best fashion online store</p>
+            <Formik action="">
+              {
+                (form) => (
+                  <Form>
+                   <LoginInput icon="email"/>
+                  </Form>
+                )
+              }
+            </Formik>
           </div>
         </div>
       </div>
