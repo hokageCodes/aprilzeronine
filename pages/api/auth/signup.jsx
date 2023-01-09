52d49da111d9371d250ad35ca9525060d8975114
hokageCodes <hashsenju94@gmail.com>
2023-01-09 19:06:12 +0100

    Postman

---
+++ pages/api/auth/signup.jsx
@@ -0,0 +1,15 @@
+import nc from 'next-connect'
+import db from '../../../utils/db'
+
+const handler = nc();
+
+handler.post(async(req, res) => {
+    try {
+        await db.connectDb();
+        console.log(req.body);
+    } catch (error) {
+        res.statusCode(500).json({message: error.message})
+    }
+})
+
+export default handler;
