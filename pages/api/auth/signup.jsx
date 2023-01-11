1612cdf6ebfc8fb38dfd31d2c74a17a03fc7149a
hokageCodes <hashsenju94@gmail.com>
2023-01-11 18:25:23 +0100

    Still on validation

--- pages/api/auth/signup.jsx
+++ pages/api/auth/signup.jsx
@@ -1,4 +1,5 @@
 import nc from 'next-connect'
+import bcrypt from 'bcrypt'
 import db from '../../../utils/db'
 import { validateEmail } from '../../../utils/validation';
 import {User} from '../../../models/User'
@@ -22,8 +23,21 @@ handler.post(async(req, res) => {
         if (password.length < 6) {
             return res
                 .status(400)
-                .json({ message: "Password must be atleast 6 characters." });
+                .json({ message: "Password must be at least 6 characters." });
         }
+        const cryptedPassword = await bcrypt.hash(password, 12);
+        const newUser = new User({ name, email, password: cryptedPassword });
+
+        const addedUser = await newUser.save();
+        const activation_token = createActivationToken({
+            id: addedUser._id.toString(),
+        });
+        const url = `${process.env.BASE_URL}/activate/${activation_token}`;
+        sendEmail(email, url, "", "Activate your account.", activateEmailTemplate);
+        await db.disconnectDb();
+        res.json({
+            message: "Register success! Please activate your email to start.",
+        });
     } catch (error) {
         res.status(500).json({message: error.message})
     }
